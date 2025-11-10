282e59b77a6c2f13ddd5b5bb58c3200d7038d461
Ranuthi Pehansa <ranuthi2004@gmail.com>
2025-11-10 22:39:34 +0530

    Added OrderDTO

---
+++ dto/OrderDTO.js
@@ -0,0 +1,9 @@
+class OrderDTO {
+    constructor(id, date, customer, items, total) {
+        this.id = id;
+        this.date = date;
+        this.customer = customer;
+        this.items = items;
+        this.total = total;
+    }
+}
