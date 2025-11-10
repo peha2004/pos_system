279cdb7df0cb6ae72c82aac57bcd2a056270c325
Ranuthi Pehansa <ranuthi2004@gmail.com>
2025-11-10 20:44:23 +0530

    Added itemModel

---
+++ model/itemModel.js
@@ -0,0 +1,16 @@
+// itemModel.js
+const ItemModel = {
+    getAll() {
+        return DB.items;
+    },
+    add(item) {
+        DB.items.push(item);
+    },
+    update(code, data) {
+        const i = DB.items.findIndex(it => it.code === code);
+        if (i > -1) DB.items[i] = { ...DB.items[i], ...data };
+    },
+    delete(code) {
+        DB.items = DB.items.filter(it => it.code !== code);
+    }
+};
