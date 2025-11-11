c61be7d9681c3cf5ecb1bc984d9395814defea33
Ranuthi Pehansa <ranuthi2004@gmail.com>
2025-11-11 19:05:12 +0530

    Updated CustomerModel with DTO

--- model/CustomerModel.js
+++ model/CustomerModel.js
@@ -3,13 +3,16 @@ const CustomerModel = {
     getAll() {
         return DB.customers;
     },
-    add(data) {
-        data.id = DB.nextCustomerIdGen();
-        DB.customers.push(data);
+    add(customerDTO) {
+        if (!customerDTO.id) {
+            customerDTO.id = DB.nextCustomerIdGen();
+        }
+        DB.customers.push(customerDTO);
     },
-    update(id, data) {
+
+    update(id, customerDTO) {
         const i = DB.customers.findIndex(c => c.id === id);
-        if (i > -1) DB.customers[i] = { ...DB.customers[i], ...data };
+        if (i > -1) DB.customers[i] = customerDTO;
     },
     delete(id) {
         DB.customers = DB.customers.filter(c => c.id !== id);
